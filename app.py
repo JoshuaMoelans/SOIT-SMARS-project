720dda3130658705a237e68e7921e7e28d73a4cc
JoshuaMoelans <joshua.moelans@student.uantwerpen.be>
2022-04-01 15:40:49 +0200

    Added file download to download button and removed unnecessary files

--- app.py
+++ app.py
@@ -9,6 +9,17 @@ app = Flask(__name__)
 def interactJS():
     return render_template('interactJS.html')
 
+# TODO add network information to file output dynamically
+# labels: back-end
+@app.route("/getCodeOutput")
+def getCodeOutput():
+    pyFile = 'if __name__ == \'__main__\': \n\tprint(\'Hello World!\')'
+    return Response(
+        pyFile,
+        mimetype="text/csv",
+        headers={"Content-disposition":
+                     "attachment; filename=myCode.py"})
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=random.randint(2000, 9000))
+    app.run(host='0.0.0.0', port=random.randint(2000, 9000))
